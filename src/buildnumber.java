428871ae5cf0f954ac5f24fbb61a15c157d57028
gaya3 <sucharithamb@gmail.com>
2023-06-06 13:23:55 -0500

    p

---
+++ src/buildnumber.java
@@ -0,0 +1,26 @@
+package newproject;
+import java.util.Scanner;
+
+public class buildnumber {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number");
+        int number = sc.nextInt();
+        int num1 = 0, num2 = 1;
+
+        int count = 0;
+
+        while (count < number) {
+
+
+            System.out.print(num1 + " ");
+
+            int num3 = num2 + num1;
+            num1 = num2;
+            num2 = num3;
+            count = count + 1;
+        }
+    }
+
+
+}
